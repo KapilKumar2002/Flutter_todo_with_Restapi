2052eb26cfbeaa93a3a4ee6e9cc9f488054a48fc
KapilKumar2002 <kapil012002@gmail.com>
2022-11-24 23:30:28 +0530

    af

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Constants {
   static Color themeColor = Color.fromARGB(221, 39, 225, 231);
 
-  static String url = 'http://10.0.2.2:8000';
-  // static String url = 'http://127.0.0.1:33103/#/';
+  // static String url = 'http://10.0.2.2:8000';
+  static String url = 'https://limitless-citadel-45775.herokuapp.com';
 }
